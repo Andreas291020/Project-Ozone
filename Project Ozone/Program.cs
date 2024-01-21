f4be15b74e9f81707da3951667ef35a89dddda63
Andreas291020 <andreas.haugen@gokstadakademiet.no>
2024-01-21 15:24:37 +0100

    Added a function that counts to 5

--- Project Ozone/Program.cs
+++ Project Ozone/Program.cs
@@ -6,5 +6,16 @@ class Program
     {
         Console.WriteLine("Hello, Git!");
         Console.WriteLine("I am Andreas");
+
+        CountTo(num: 5);
+    }
+
+    static void CountTo(int num)
+    {
+        Console.WriteLine($"Counting to {num}");
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine(i+"...");
+        }
     }
 }
